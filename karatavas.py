c4e320f7f75a1c008f7e9d2dc7c86ec978597a2f
mrtswd <60158608+mrtswd@users.noreply.github.com>
2020-10-10 12:20:25 +0300

    Add files via upload

---
+++ karatavas.py
@@ -0,0 +1,140 @@
+import random
+from words import word_list # words failam jabut taja pasha mapee kur py fails
+
+# words to uppercase
+def get_words():
+    word = random.choice(word_list)
+    return word.upper()
+
+# definejam attelot tuksumus un meginajumu skaitu
+def play(word):
+    word_comp = "_" * len(word)
+    guessd = False
+    guess_lett = []
+    guess_words = []
+    tries = 6
+    print("Uzspēlēsim karātavu jeb vārdu spēli! Bet ņem vērā, ka vārdi ir angļu valodā!")
+    print(display_hangman(tries))
+    print(word_comp)
+    print("\n")
+    while not guessd and tries > 0:
+        guess = input("Lūdzu mini kādu burtu:  ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guess_lett:
+                print("Tu jau šādu burtu minēji: ", guess)
+            elif guess not in word:
+                print("Diemžēl", guess, " burts nav šajā vārdā")
+                tries -= 1
+                guess_lett.append(guess)
+            else:
+                print("Tu uzminēji: ", guess, "burts ir šajā vārdā!")
+                guess_lett.append(guess)
+                word_as_list = list(word_comp)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_comp = "".join(word_as_list)
+                if "_" not in word_comp:
+                    guessd = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guess_words:
+                print("Tu jau uzminēji šo burtu iepriekš", guess)
+            elif guess != word:
+                print(guess, "nav šāda burta minamajā vārdā")
+                tries -= 1
+                guess_words.append(guess)
+            else:
+                guessd = True
+                word_comp = word
+        else:
+            print("Nederīgs minējums.")
+        print(display_hangman(tries))
+        print(word_comp)
+        print("\n")
+    if guessd:
+        print("Apsveicam! Tu uzminēji vārdu. Tu vinnēji!!!!")
+    else:
+        print("Diemžēl visas iesp­ējas izt­ērētas. Vārds bija: " + word + ". Varbūt veiksies nākamajā reizē!")
+
+# draw man in terminal
+def display_hangman(tries):
+    stages = [  # pedejais
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """ 
+                ,
+                # iepriekspedejais
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # pirmais
+                """ 
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+def main():
+    word = get_words()
+    play(word)
+    while input("Spēlēsim vēlreiz? (Y / N) ").upper() == "Y":
+        word = get_words()
+        play(word)
+
+if __name__ == "__main__":
+    main()
